c782c97b76ac7e2c4c52c9e39aac52ee9d2a2d61
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-22 17:13:25 -0800

    Modified: POP First Element Is Deleted

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -27,14 +27,20 @@ public class LinkedList {
         previousNode.next = newNode;
 
     }
-
+	 public void pop(){
+	        if(head==null){
+	            System.out.println("List is Empty and Can't be delete");
+	        }
+	        System.out.println("Deleting the first element");
+	        head=head.next;
+	    }
 	public void display() {
 		Node current = head;
 		if (head == null) {
 			System.out.println("List is Empty");
 			return;
 		} else {
-			System.out.println("Nodes of simple linked list:");
+
 			while (current != null) {
 				System.out.println(current.data + " ");
 				current = current.next;
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -4,10 +4,15 @@ public class LinkedListMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Linked List Program");
-		LinkedList list=new LinkedList();
+		System.out.println("Nodes of simple linked list:");
+		LinkedList list = new LinkedList();
 		list.add(56);
 		list.add(70);
+		list.display();
+		System.out.println("30 is Inserting between 56 and 70");
 		list.insert(list.head, 30);
+		list.display();
+		list.pop();
 		list.display();
 	}
 
