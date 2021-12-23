b976ea13a5161da3974aedc1f41e06b2ba073f56
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-22 16:34:50 -0800

    Refactor: data 30 & 56 to 70

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -5,9 +5,9 @@ public class LinkedListMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Linked List Program");
 		LinkedList list=new LinkedList();
-		list.add(56);
-		list.add(30);
 		list.add(70);
+		list.add(30);
+		list.add(56);
 		list.display();
 	}
 
