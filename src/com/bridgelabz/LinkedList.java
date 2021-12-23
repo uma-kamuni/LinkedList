427087459d3590f2396bda82fbeb97ba26c82599
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-22 16:25:43 -0800

    Added: data 30 & 56 to 70

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -7,28 +7,29 @@ public class LinkedList {
 
 	public void add(int data) {
 		Node newnode = new Node(data);
-				if (head == null) {
-					this.node = newnode;
-					this.head = newnode;
-					this.tail = newnode;
-				}else {
-					tail.next = newnode;
-							tail = newnode;
-				}
+		if (head == null) {
+			this.node = newnode;
+			this.head = newnode;
+			this.tail = newnode;
+		} else {
+			Node temp = head;
+			this.head = newnode;
+			this.head.next = temp;
+		}
 	}
 
-public void display() {
-	Node current = head;
-	if(head == null) {
-		System.out.println("List is Empty");
-	return;
-} else	{
-		System.out.println("Nodes of simple linked list:");
-		while (current != null) {
-			System.out.println(current.data + " ");
-			current = current.next;
+	public void display() {
+		Node current = head;
+		if (head == null) {
+			System.out.println("List is Empty");
+			return;
+		} else {
+			System.out.println("Nodes of simple linked list:");
+			while (current != null) {
+				System.out.println(current.data + " ");
+				current = current.next;
+			}
+			System.out.println();
 		}
-		System.out.println();
 	}
-}
 }
